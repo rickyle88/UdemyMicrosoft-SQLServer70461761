d0569e64e569fb3e2e5afc6cfadf9f6449671fab
NguyenLe <bsbnguyen@gmail.com>
2021-03-19 23:35:29 -0700

    First commit

---
+++ Section02/Section03.sql
@@ -0,0 +1,49 @@
+--Create table and column
+--USE X
+--CREATE TABLE tblSecond
+--(myNumbers int);
+--GO
+
+--Insert data
+--INSERT INTO tblSecond
+--VALUES (234)
+
+--INSERT INTO tblSecond
+--VALUES (456),
+--(156),
+--(478)
+
+--SELECT *
+--FROM [dbo].[tblFirst];
+
+--SELECT *
+--FROM tblSecond
+
+--SELECT TeamID as ID, TeamName as Name
+--FROM SportTeams;
+
+--SELECT *
+--FROM [dbo].[tblFirst];
+
+--Delete table content
+--DELETE
+--FROM tblFirst
+
+--
+--TRUNCATE TABLE tbl
+
+--
+--
+--DROP TABLE tblFirst
+
+--SELECT *
+--FROM [dbo].[tblSecond];
+
+--Excercise
+CREATE TABLE tblPrimeNumbers
+(intField int)
+
+INSERT INTO tblPrimeNumbers
+VALUES (2),(3),(5),(7),(11)
+DELETE FROM tblPrimeNumbers
+DROP TABLE tblPrimeNumbers
---
+++ Section04NumbersFunctions/SQLQuery1.sql
@@ -0,0 +1,6 @@
+USE [70-461]
+GO
+
+--Create in 70-461 database
+CREATE TABLE tblEmployee
+(EmployeeNumber int, EmployeeName int)
